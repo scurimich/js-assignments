acea3166258c58f65b21d2045e8437abe7b690a2
Tsykota Vlad <tsykota.vlad@gmail.com>
2016-03-22 14:12:36 +0300

    Fixed #3

--- task/01-strings-tasks.js
+++ task/01-strings-tasks.js
@@ -255,7 +255,7 @@ function encodeToRot13(str) {
  *   isString(new String('test')) => true
  */
 function isString(value) {
-    return (typeof value === 'string' || value instanceof String) ? true : false;
+    return typeof value === 'string' || value instanceof String;
 }
 
 
